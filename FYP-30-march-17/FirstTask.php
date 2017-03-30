b17baccdfd8c766e7ad912da4dca33f5da86b5d5
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-03-31 01:25:24 +0500

    check data type with the help

--- FYP-30-march-17/FirstTask.php
+++ FYP-30-march-17/FirstTask.php
@@ -2,7 +2,21 @@
 <body>
 
 <?php
-echo " My first PHP script!";
+$zeeshan = "Khan";
+
+$y = 'das';
+var_dump($y);
+echo "<br>";
+function myfunct(){
+	static $a = 5;
+	echo "".++$a;
+		;
+}
+myfunct();
+myfunct();
+echo " My first PHP script! ";
+
+print $y + $y;
 ?>
 
 </body>
