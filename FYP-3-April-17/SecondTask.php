4ee7ecd86a7c910da2e1100fa0f37ead704ae65b
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-04-07 04:57:15 +0500

    some error on gender index say undefined error

--- FYP-3-April-17/SecondTask.php
+++ FYP-3-April-17/SecondTask.php
@@ -2,13 +2,29 @@
 <html>
 <head>
 	<title>Faom</title>
+	<style>
+.error {color: #FF0000;}
+</style>
 </head>
 
 <?php 
-$name = $age = "";
+$Err = " is required";
+$fname = $lname = $fathername =  $age = $gender = $email = $cellnum = "";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-	$name = get_val($_POST["name"]);
+	$fname = get_val($_POST["fname"]);
+	
+	$lname = get_val($_POST["lname"]);
+
+	$fathername = get_val($_POST["fathername"]);
+	
 	$age = get_val($_POST["age"]);
+
+	$email = get_val($_POST["e_mail"]);
+ 
+    $cellnum = get_val($_POST["phone_num"]);
+   // $gender = get_val($_POST["gender"]);
+
+
 }
 function get_val($data){
 	$data = trim($data);
@@ -20,13 +36,25 @@ function get_val($data){
 
 <body>
 <center>
-<H1>Heading</H1>
+<H1>Add Student Record</H1>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-Name : <input type="text" name="name"><br>
-Age  : <input type="text" name="age"><br>
+First Name  : <input type="text" name="fname" required><br>
+Last Name   : <input type="text" name="lname" required=><br>
+Father Name : <input type="text" name="fathername" required><br>
+Age         : <input type="text" name="age"><br>
+Gender:
+              <input type="radio" name="gender" value="female">Female
+              <input type="radio" name="gender" value="male">Male<br>
+Email       : <input type="text" name="e_mail"><br>
+Cell Phone# : <input type="text" name="phone_num" required><br>
+
 <input type="submit"><br>
 
 </form></center>
-<?php echo $name." ".$age;?>
+<?php echo $fname." ".$age;
+$fname = ' ';
+$age = ' ';
+?>
+
 </body>
 </html>
