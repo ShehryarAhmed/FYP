ff315fe95d63227058912c20bdff86bc7b83d218
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-04-07 03:49:30 +0500

    create function

--- FYP-3-April-17/SecondTask.php
+++ FYP-3-April-17/SecondTask.php
@@ -3,14 +3,30 @@
 <head>
 	<title>Faom</title>
 </head>
+
+<?php 
+$name = $age = "";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	$name = get_val($_POST["name"]);
+	$age = get_val($_POST["age"]);
+}
+function get_val($data){
+	$data = trim($data);
+	$data = stripcslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+?>
+
 <body>
 <center>
 <H1>Heading</H1>
-<form action="getvalue.php" method="get">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 Name : <input type="text" name="name"><br>
 Age  : <input type="text" name="age"><br>
-Email : <input type="text" name="email"><br>
 <input type="submit"><br>
+
 </form></center>
+<?php echo $name." ".$age;?>
 </body>
 </html>
--- FYP-3-April-17/getvalue.php
+++ FYP-3-April-17/getvalue.php
@@ -1,7 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body>
-Name <?php echo $_GET["name"]; ?><br>
-Age <?php echo $_GET["age"]; ?>
-</body>
-</html>
+<
