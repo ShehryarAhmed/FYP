bc0b953f03cd5d91a5f658e122c4858e72415bf4
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-04-07 01:52:56 +0500

    second task

---
+++ FYP-3-April-17/SecondTask.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Faom</title>
+</head>
+<body>
+<center>
+<H1>Heading</H1>
+<table ><tr>
+Name : <input type="text" name="name"><br>
+Age  : <input type="text" name="age"><br>
+Email : <input type="text" name="email"><br>
+<input type="submit" name=""><br>
+</tr></table></center>
+</body>
+</html>
