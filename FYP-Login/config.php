048fc6b3b22f627aeddbdf56518749c4431bdbdb
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-10-01 23:06:53 +0500

    add conifg file

---
+++ FYP-Login/config.php
@@ -0,0 +1,9 @@
+<?php
+define('serverName','localhost');
+define('userName','root');
+define('password','');
+define('db','login');
+$connectDB = mysqli_connect(serverName,userName,password,db);
+
+
+?>
